b7ea9a72b77bc4e6fa58b1204043f7ac12ee492b
TheSpahThatSpies <christianborggaard@gmail.com>
2023-02-28 22:06:12 +0100

    Projekter

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import mypicture from './img/me.png';
+import futurepic from './img/futureworld.png'
+import studypic from './img/studyroom.png'
 
 function App() {
   return (
     <div className="App">
-      <main class="flex bg-black h-screen flex-col content-center justify-center">
+      <main class="pt-16 flex bg-black flex-col content-center justify-center">
         <div class="flex space-x-28 justify-center">
           <div class='bg-pink px-10 py-7'>
             <h1 class="text-7xl text-white font-sans font-bold">Christian Borggård</h1>
@@ -19,6 +21,15 @@ function App() {
             <p class="mt-2 text-2xl text-white text-left">Telefon: +45 23659693</p>
           </div>
           <img src={mypicture} alt="me" class="w-1/3"></img>
+        </div>
+        <h1 class="text-8xl mt-20 text-white font-sans font-bold">Tidligere projekter:</h1>
+        <div class="bg-black mt-16 mb-16 flex content-center justify-center ml-36">
+          <a href="https://futureworld.vercel.app/home">
+            <img src={futurepic} alt="futureworld" class="w-3/4"></img>
+          </a>
+          <a href="https://studyroom-gamma.vercel.app/home">
+            <img src={studypic} alt="studyroom" class="w-3/4"></img>
+          </a>
         </div>
       </main>
     </div>
